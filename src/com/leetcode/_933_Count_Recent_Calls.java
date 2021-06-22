b949df6c9f60c900867f38277a3c2c65f10ceb48
SahinSarkar <sarkarsahin697971@gmail.com>
2021-06-22 16:11:29 +0530

    Moved question to cvent package

---
+++ src/com/leetcode/_933_Count_Recent_Calls.java
@@ -0,0 +1,20 @@
+package com.leetcode;
+
+public class _933_Count_Recent_Calls {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class RecentCounter {
+
+    public RecentCounter() {
+
+    }
+
+    public int ping(int t) {
+
+    }
+}
+
